5ee6d8e4bfdbb1ff5c4cc0a543f879e6ac3d5d34
Jose Luis <luisj.valdiviezo20@gmail.com>
2023-02-10 10:31:32 -0500

    Add: database

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 /* Protocol Conection */
+const { Database } = require('./Mysql');
+const net = require('node:net');
+
+const database = Database({
+  host: 'localhost',
+  user: 'root',
+  database: 'sch_spbmaxweb',
+  password: 'Solucionespb2.',
+})
+
+database.connect((err) => {
+  if (err) {
+    database.end();
+    database.destroy();
+  };
+});
+
+console.log(database);
