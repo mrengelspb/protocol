453bf9feccd283d166fee6b5c09b113f7f07784b
Joseph <luisj.valdiviezo20@gmail.com>
2023-02-28 23:55:14 -0500

    mounting electron

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -4,7 +4,7 @@ import '../styles/header.css';
 export default function Header() {
   return (
     <header className="header">
-      <img className="header--logo" src="/assets/logo.svg" alt="Your SVG" />
+      <img className="header--logo" src="./assets/logo.svg" alt="Your SVG" />
       <div className="header--local">Parqueadero</div>
       <div className="menu--operador">
       </div>
